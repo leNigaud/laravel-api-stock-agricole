b1d6481f0e893403f72e90fa07866dda85a5937f
Ginaud Rafenoarimanana <111077740+leNigaud@users.noreply.github.com>
2024-09-01 20:11:53 +0000

    Update index.php 'cause error

--- api/index.php
+++ api/index.php
@@ -5,9 +5,11 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
  
 // Run the application
+/*
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 $response->send();
 $kernel->terminate($request, $response);
+*/
